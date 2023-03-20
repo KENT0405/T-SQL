74669a83e105c470a7d8a9826e0687104223a464
kent <faec9408cc@gmail.com>
2023-03-20 17:10:44 +0800

    2023-03-20

--- #up_bo_game_profitlose.sql
+++ #up_bo_game_profitlose.sql
@@ -1,225 +1,113 @@
 /*
-declare @p16 int
-set @p16=83254
-exec sp_executesql N'EXEC [dbo].[#up_bo_game_profitlose]  @P0, @P1, @P2, @P3, @P4, @P5, @P6, @P7, @P8, @P9, @P10, @P11, @P12 ,@P13 OUT',
-N'@P0 datetime2,@P1 datetime2,@P2 nvarchar(4000),@P3 nvarchar(4000),@P4 bit,@P5 bit,@P6 nvarchar(4000),@P7 nvarchar(4000),@P8 nvarchar(4000),@P9 nvarchar(4000),@P10 int,@P11 int,@P12 int,@P13 int OUTPUT',
-'2023-03-06 00:00:00','2023-03-12 23:59:59.9970000',N'',N'-',1,0,N'',N'',N'',N'',1,1,20,@p16 output
-select @p16
+declare @p15 int
+set @p15=NULL
+exec sp_executesql N'EXEC [dbo].[up_bo_game_profitlose]  @P0, @P1, @P2, @P3, @P4, @P5, @P6, @P7, @P8, @P9, @P10, @P11, @P12 OUT',
+N'@P0 datetime2,@P1 datetime2,@P2 varchar(8000),@P3 bit,@P4 bit,@P5 varchar(8000),@P6 varchar(8000),@P7 varchar(8000),@P8 varchar(8000),@P9 varchar(8000),@P10 int,@P11 int,@P12 int OUTPUT',
+'2023-03-01 00:00:00','2023-03-19 23:59:59.9970000','-',1,1,'','','','','',1,20,@p15 output
+select @p15
 GO
+
+declare @p15 int
+set @p15=NULL
+exec sp_executesql N'EXEC [dbo].[#up_bo_game_profitlose]  @P0, @P1, @P2, @P3, @P4, @P5, @P6, @P7, @P8, @P9, @P10, @P11, @P12 OUT',
+N'@P0 datetime2,@P1 datetime2,@P2 varchar(8000),@P3 bit,@P4 bit,@P5 varchar(8000),@P6 varchar(8000),@P7 varchar(8000),@P8 varchar(8000),@P9 varchar(8000),@P10 int,@P11 int,@P12 int OUTPUT',
+'2023-03-01 00:00:00','2023-03-19 23:59:59.9970000','-',1,1,'','','','','',1,20,@p15 output
+select @p15
 */
--- =====================================================================================
--- Author:		<zhen.fang>
--- Create date: <2019-09-19>
--- Description:	<backoffice game profitlose>
--- =====================================================================================
--- exec up_bo_game_profitlose '2021-12-01 00:00:00','2021-12-30 23:59:00','BBIN','-',1,1,'','','','',1,5
+-- =============================================
+-- Author:		<kent/luna>
+-- Create date: <2022-12-13>
+-- Description:	<up_bo_game_profitlose_include_downline>
+-- =============================================
+-- exec up_bo_game_profitlose_include_downline '2020-10-10 00:00:00','2020-10-20 23:59:59','-',1,0,'SM','Web','PTS','S-LY01'
+
 CREATE OR ALTER PROCEDURE [dbo].[#up_bo_game_profitlose]
 	@BeginDate		DATETIME = '',
 	@EndDate		DATETIME = '',
-	@ServerCode		VARCHAR(10) = '',
-	@MerchantCode	VARCHAR(MAX) = '',  -- MerchantCode : - for all split(TEST,SKY777)
+	@MerchantCode	VARCHAR(10) = '',
 	@IsAll			BIT = 0,
 	@SumGroup		BIT = 0,
-	@GameCode		VARCHAR(2) = '',
+	@GameCategory	VARCHAR(2) = '',
 	@Channel		VARCHAR(10) = '',
 	@Currency		VARCHAR(10) = '',
-	@Game			VARCHAR(10) = '',
-	@Type			INT = 1,  --1=ORDER BY Bet Amt,2= ORDER BY BETS,3=ORDER BY ACTICE PLAYER
+	@GameCode		VARCHAR(10) = '',
+	@Provider		VARCHAR(10) = '',
 	@PageIndex		INT = 1,
 	@PageSize		INT = 10,
 	@RecordsCount	INT = 0 OUTPUT
 AS
 BEGIN
-	SET NOCOUNT, ARITHABORT ON;
+	SET NOCOUNT ,ARITHABORT ON;
 
-	DECLARE
-		@SQL			NVARCHAR(MAX) = '',
-		@SQL_CODE		NVARCHAR(100) = '',
-		@SQL_MR			NVARCHAR(MAX) = '',
-		@SQL_condition	NVARCHAR(MAX) = '',
-		@SQL_TABLE		NVARCHAR(100) = '',
-		@TableName		NVARCHAR(100) = ''
+	DROP TABLE IF EXISTS #acct_game;
+	DROP TABLE IF EXISTS #merchant_relation;
 
-	DROP TABLE IF EXISTS #temp;
-	DROP TABLE IF EXISTS #temp_main;
+	CREATE TABLE #merchant_relation
+	(
+		merchant_code		VARCHAR(50),
+		sub_merchant_code	VARCHAR(50)
+	)
 
-	SET @SQL_TABLE = N' mdr.Merchant_Code ';
-	SET @SQL_CODE = N' mdr.Merchant_Code,';
-
-	IF(@MerchantCode = '-' AND @IsAll = 1 AND @SumGroup = 0)
+	IF (@IsAll = 1)
 	BEGIN
-		SET @MerchantCode = '';
-		SET @IsAll = 0;
-	END
-
-	SELECT @TableName = dbo.FN_GetTableNameByServerCode(@ServerCode,'game');
-	DECLARE @CharCount INT;
-	SET @CharCount =  CHARINDEX(',',@MerchantCode)
-
-	IF( @ServerCode <> '' )
-	BEGIN
-		SET @SQL_condition += N'AND server_code = @ServerCode'
-	END
-
-	IF (@IsAll = 1 )
-	BEGIN
-		IF (@CharCount = 0)
-		BEGIN
-			SET @SQL_MR +=N'
-			SELECT DISTINCT sub_merchant_code
-			INTO #temp
-			FROM merchant_relation WITH(NOLOCK)
-			WHERE merchant_Code = @MerchantCode'
-		END
-		ELSE
-		BEGIN
-			SET @SQL_MR +=N'
-			SELECT DISTINCT sub_merchant_code
-			INTO #temp
-			FROM merchant_relation WITH(NOLOCK)
-			WHERE merchant_Code in(select * from FN_StrToTable (REPLACE(REPLACE('''+@MerchantCode+ ''',''['',''''),'']'','''')))'
-		END
-
-		IF( @ServerCode <> '' )
-		BEGIN
-			SET @SQL_MR += N'AND server_code = @ServerCode'
-		END
-
-		SET @SQL_condition += N'AND merchant_Code IN (SELECT sub_merchant_code FROM #temp)'
-
-		IF (@SumGroup = 1 )
-		BEGIN
-			SET @SQL_TABLE = N' ''-'' ';
-			SET @SQL_CODE = ' ';
-		END
+		INSERT INTO #merchant_relation(merchant_code,sub_merchant_code)
+		SELECT
+			merchant_code,
+			sub_merchant_code
+		FROM dbo.merchant_relation mr WITH (NOLOCK)
+		WHERE mr.Merchant_Code = @MerchantCode
 	END
 	ELSE
 	BEGIN
-		IF ( @MerchantCode<>'')
-		BEGIN
-			IF (@CharCount = 0)
-			BEGIN
-				SET @SQL_condition +=N' AND merchant_code = @MerchantCode '
-			END
-			ELSE
-			BEGIN
-				SET @SQL_condition +=N' AND merchant_code in(select * from FN_StrToTable (REPLACE(REPLACE('''+@MerchantCode+ ''',''['',''''),'']'',''''))) '
-			END
-		END
+		INSERT INTO #merchant_relation(merchant_code,sub_merchant_code)
+		SELECT '', @MerchantCode
 	END
-
-	IF ( @Game <> '' )
-	BEGIN
-		SET @SQL_condition += N' AND mdr.game_code = @Game '
-	END
-
-	IF ( @Channel <> '' )
-	BEGIN
-		SET @SQL_condition += N' AND mdr.channel = @Channel '
-	END
-
-	IF ( @Currency <> '' )
-	BEGIN
-		SET @SQL_condition += N' AND mdr.curr_id = @Currency '
-	END
-
-	IF ( @GameCode <> '' )
-	BEGIN
-		SET @SQL_condition += N' AND mdr.game_category = @GameCode '
-	END
-
-	SET @SQL =
-	@SQL_MR + '
-	SELECT * INTO #temp_main
-	FROM central_acct_game_daily_tran mdr WITH(NOLOCK)
-	WHERE mdr.tran_date between @BeginDate and @EndDate'
-	+ @SQL_condition  + '
 
 	SELECT
-		'+ @SQL_TABLE + 'as MerchantCode,
-		mdr.server_code as ServerCode,
-		mdr.curr_id as CurrencyId,
-		mdr.game_category as GameCode,
-		mdr.game_code as BetType,
-		SUM(mdr.bet_count) as TotalBetNum,
-		SUM(mdr.ttl_bet) as TotalBetAmount,
-		SUM(mdr.success_bet) as TotalValidBet ,
-		SUM(mdr.wl_amt) as TotalWL,
-		COUNT(distinct mdr.login_id +''@''+merchant_code) as TotalPlayer,
-		SUM(mdr.jp_contribute_amt) as JPContributeAmt,
-		SUM(mdr.jp_win) as JpWin,
-		SUM(mdr.success_bet * real_curr_rate_after) AS MainValidBetAmount,
-		SUM(mdr.jp_contribute_amt * real_curr_rate_after) AS MainJPContributeAmt,
-		SUM(mdr.wl_amt * real_curr_rate_after) AS MainTotalWL,
-		mdr.server_code
-	FROM #temp_main as mdr WITH (NOLOCK)
-	INNER JOIN sys_currency_log AS scl WITH(NOLOCK)
-	ON scl.curr_id = mdr.curr_id
-	AND scl.curr_status = 1
-	AND scl.curr_month = CONVERT(varchar(7),mdr.tran_date,120)
-	AND mdr.tran_date >= curr_mth_start
-	AND mdr.tran_date < curr_mth_end
-	GROUP BY ' + @SQL_CODE + ' mdr.curr_id,mdr.game_category, mdr.game_code, mdr.server_code
-	ORDER BY ServerCode ASC,'
-	+ CASE @Type
-		WHEN 1 THEN ' TotalValidBet DESC'
-		WHEN 2 THEN ' TotalBetNum DESC'
-		WHEN 3 THEN ' TotalPlayer DESC' END + '
+		CASE WHEN @IsAll = 1 THEN
+			CASE WHEN @SumGroup = 1
+			THEN mr.Merchant_Code
+			ELSE mdr.Merchant_Code END
+		ELSE mdr.Merchant_Code END  AS merchant_code,
+		mdr.curr_id					AS curr_id,
+		mdr.game_category			AS game_category,
+		mdr.game_code				AS game_code,
+		SUM(mdr.bet_count)			AS bet_count,
+		SUM(mdr.ttl_bet)			AS ttl_bet,
+		SUM(mdr.success_bet)		AS success_bet,
+		SUM(mdr.wl_amt)				AS wl_amt,
+		COUNT(DISTINCT mdr.acct_id) AS total_player,
+		SUM(mdr.jp_contribute_amt)	AS jp_contribute_amt,
+		SUM(mdr.jp_win)				AS jp_win,
+		provider
+	INTO #acct_game
+	FROM acct_game_daily_tran AS mdr WITH (NOLOCK)
+	INNER JOIN #merchant_relation mr
+	ON mdr.merchant_code = mr.sub_merchant_code
+	WHERE mdr.tran_date BETWEEN @BeginDate AND @EndDate
+	AND (mdr.game_code = @GameCode OR @GameCode = '')
+	AND (mdr.channel = @Channel OR @Channel = '')
+	AND (mdr.curr_id = @Currency OR @Currency = '')
+	AND (mdr.provider = @Provider OR @Provider = '')
+	AND (mdr.game_category = @GameCategory OR @GameCategory = '')
+	GROUP BY
+		CASE WHEN @IsAll = 1 THEN
+			CASE WHEN @SumGroup = 1
+			THEN  mr.Merchant_Code
+			ELSE  mdr.Merchant_Code  END
+		ELSE mdr.Merchant_Code END,
+		mdr.curr_id,
+		mdr.game_category,
+		mdr.game_code,
+		provider
+
+	SELECT @RecordsCount = @@ROWCOUNT
+
+	SELECT *
+	FROM #acct_game
+	ORDER BY success_bet DESC
 	OFFSET (@PageIndex - 1) * @PageSize ROWS
-	FETCH NEXT @PageSize ROWS ONLY;
+	FETCH NEXT @PageSize ROWS ONLY
 
-	SELECT @RecordsCount = COUNT( 1 )
-	FROM
-	(
-		SELECT ' + @SQL_TABLE + ' as MerchantCode
-		FROM '+@TableName+' as mdr WITH (NOLOCK)
-		WHERE mdr.tran_date between @BeginDate and @EndDate'
-		+ @SQL_condition +'
-		GROUP BY ' + @SQL_CODE + ' mdr.curr_id,mdr.game_category,mdr.game_code,mdr.server_code
-	) q;
-
-	SELECT
-		COUNT(distinct mdr.login_id +''@''+merchant_code) as TotalPlayer,
-		SUM(mdr.bet_count) as TotalBetNum,
-		SUM(mdr.success_bet * real_curr_rate_after) AS MainValidBetAmount,
-		SUM(mdr.jp_contribute_amt * real_curr_rate_after) AS MainJPContributeAmt,
-		SUM(mdr.wl_amt * real_curr_rate_after) AS MainTotalWL
-	FROM #temp_main mdr WITH(NOLOCK)
-	INNER JOIN sys_currency_log AS scl WITH(NOLOCK)
-	ON scl.curr_id = mdr.curr_id
-	AND scl.curr_status = 1
-	AND scl.curr_month = CONVERT(VARCHAR(7),mdr.tran_date,120)
-	AND mdr.tran_date >= curr_mth_start
-	AND mdr.tran_date < curr_mth_end
-	'
-
-	PRINT @SQL
-
-    EXEC sp_executesql @SQL,N'
-	@BeginDate		DATETIME,
-	@EndDate		DATETIME,
-	@ServerCode		VARCHAR(10),
-	@MerchantCode	VARCHAR(20),
-	@Currency		VARCHAR(20),
-	@Channel		VARCHAR(10),
-	@GameCode		VARCHAR(10),
-	@game			VARCHAR(10),
-	@PageIndex		INT,
-	@PageSize		INT,
-	@RecordsCount	INT OUTPUT',
-	@BeginDate,
-	@EndDate,
-	@ServerCode,
-	@MerchantCode,
-	@Currency,
-	@Channel,
-	@GameCode,
-	@game,
-	@PageIndex,
-	@PageSize,
-	@RecordsCount OUTPUT;
-
-	SET NOCOUNT, ARITHABORT OFF;
+	SET NOCOUNT ,ARITHABORT OFF;
 END
-
-
