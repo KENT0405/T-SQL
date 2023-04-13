cb656f32dba87f44f523f127cb8dff9dee7d5b07
kent <faec9408cc@gmail.com>
2023-04-13 11:19:41 +0800

    2023-04-13

--- Script/Check_table_index.sql
+++ Script/Check_table_index.sql
@@ -6,7 +6,7 @@ SELECT
         SELECT QUOTENAME(c.name) + CASE WHEN ic.is_descending_key = 1 THEN ' DESC' ELSE '' END AS [data()]
         FROM sys.index_columns AS ic
         INNER JOIN sys.columns AS c ON ic.object_id = c.object_id AND ic.column_id = c.column_id
-        WHERE ic.object_id = i.object_id AND ic.index_id = i.index_id AND ic.is_included_column = 0
+        WHERE ic.object_id = i.object_id AND ic.index_id = i.index_id AND ic.is_included_column = 0 AND key_ordinal > 0
         ORDER BY ic.key_ordinal
         FOR XML PATH
     ), '<row>', ', '), '</row>', ''), 1, 2, '') AS KeyColumns,
