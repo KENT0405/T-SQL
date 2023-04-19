0fa5e757f6936e5079760e051d72386973eb87bd
kent <faec9408cc@gmail.com>
2023-04-19 17:20:39 +0800

    2023-04-19

--- Script/Check Event.sql
+++ Script/Check Event.sql
@@ -1,3 +1,6 @@
+---------------------------------------------------------------
+------------------------Check_timeout--------------------------
+---------------------------------------------------------------
 SELECT
     --n.value('(@name)[1]', 'VARCHAR(50)') AS event_name,
     --n.value('(@package)[1]', 'VARCHAR(50)') AS package_name,
@@ -11,11 +14,13 @@ SELECT
     n.value('(data[@name="row_count"]/value)[1]', 'INT') AS row_count,
     n.value('(action[@name="database_name"]/value)[1]', 'NVARCHAR(128)') AS database_name,
 	n.value('(data[@name="result"]/value)[1]', 'VARCHAR(10)') AS result
-FROM (
+FROM
+(
 	SELECT CAST(event_data AS XML) AS event_data
-	FROM sys.fn_xe_file_target_read_file('D:\Events\T-SQL*.xel', null, null, null)) ed
+	FROM sys.fn_xe_file_target_read_file('D:\Events\T-SQL*.xel', null, null, null)
+) ed
 CROSS APPLY ed.event_data.nodes('event') AS q(n)
-WHERE 1=1
+WHERE 1 = 1
 AND n.value('(data[@name="statement"]/value)[1]', 'NVARCHAR(MAX)') LIKE '%declare%'
 AND n.value('(@timestamp)[1]', 'DATETIME2') >= CAST(GETDATE()-3 AS DATE)
 AND n.value('(data[@name="result"]/value)[1]', 'VARCHAR(10)') = 2
@@ -26,3 +31,17 @@ select cast(event_data as XML) as event_data
 from sys.fn_xe_file_target_read_file('D:\Events\T-SQL Trace_0_133195357183830000.xel', null, null, null)
 */
 
+---------------------------------------------------------------
+-------------------------Check_Lock----------------------------
+---------------------------------------------------------------
+SELECT n.value('(@timestamp)[1]', 'DATETIME') AS [timestamp], ed.*
+FROM
+(
+	SELECT CAST(event_data AS XML) AS event_data
+	FROM sys.fn_xe_file_target_read_file('D:\Events\Lock*.xel', null, null, null)
+) ed
+CROSS APPLY ed.event_data.nodes('event') AS q(n)
+WHERE 1 = 1
+AND n.value('(@name)[1]', 'VARCHAR(50)') = 'xml_deadlock_report'
+AND n.value('(@timestamp)[1]', 'DATETIME') >= GETDATE() - 3
+ORDER BY n.value('(@timestamp)[1]', 'DATETIME2') DESC
