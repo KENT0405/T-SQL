c55e990547f4cf5a737aaeedb09cbce261c324cb
kent <faec9408cc@gmail.com>
2023-05-15 13:48:07 +0800

    2023-05-15

--- Script/@@Rename_PK.sql
+++ Script/@@Rename_PK.sql
@@ -1,9 +1,9 @@
 SELECT
 	t.name AS table_name,
 	i.name AS PK_name,
-	'EXEC sp_rename ''' + i.name + ''', ''PK_' + t.name + ''';' AS PK_rename
+	'EXEC sp_rename N''' + i.name + ''', N''PK_' + t.name + ''';' AS PK_rename
 FROM sys.tables AS t
 JOIN sys.indexes AS i
 ON t.object_id = i.object_id
 WHERE i.name <> 'PK_' + t.name
-AND index_id = 1
+AND is_primary_key = 1
