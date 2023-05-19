5dacf3e2343999f2892ca0529e950fad38790f1b
kent <faec9408cc@gmail.com>
2023-05-19 15:50:54 +0800

    2023-05-19

--- Script/Check_table_columns.sql
+++ Script/Check_table_columns.sql
@@ -12,4 +12,20 @@ INNER JOIN syscolumns	AS col ON ob.id = col.id
 INNER JOIN systypes		AS tp ON col.xusertype = tp.xusertype
 WHERE ob.xtype = 'U'
 AND ob.name = 'game_info'
-AND col.name = 'game_name_cn'
+--AND col.name = 'game_name_cn'
+
+
+
+/*----------------------ALL COLUMNS IMFORMATION----------------------*/
+
+SELECT
+	tb.NAME AS TableName,
+	t.NAME AS ColumnType,
+	c.*
+FROM sys.all_columns AS c
+JOIN sys.types AS t
+ON c.system_type_id = t.system_type_id
+JOIN (SELECT NAME, OBJECT_ID FROM sys.tables) AS tb
+ON tb.object_id = c.object_id
+WHERE t.NAME != 'sysname'
+--AND tb.NAME = 'ticket'
