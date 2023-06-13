a5ce96f0d51448ca1aea1feb158accc58d3a5efd
kent <faec9408cc@gmail.com>
2023-06-13 13:42:54 +0800

    2023-06-12

--- Script/Check_table_columns.sql
+++ Script/Check_table_columns.sql
@@ -1,26 +1,29 @@
 SELECT
-	ob.name				AS TableName,
-    col.name			AS ColumnsName,
-    tp.name + ''		AS ColumnsType,
-	col.prec			AS ColumnsLength,
-    CASE col.isnullable
+	tb.name				AS TableName,
+	c.name				AS ColumnsName,
+	t.name 				AS ColumnType,
+	c.max_length		AS ColumnsLength, --length = -1 is (max)
+	CASE c.is_nullable
 		WHEN 1 THEN 'NULL'
-		ELSE 'NOT NOLL '
-	END					AS [IsNull]
-FROM sysobjects			AS ob
-INNER JOIN syscolumns	AS col ON ob.id = col.id
-INNER JOIN systypes		AS tp ON col.xusertype = tp.xusertype
-WHERE ob.xtype = 'U'
-AND ob.name = 'game_info'
---AND col.name = 'game_name_cn'
-
+		ELSE 'NOT NULL '
+	END					AS [IsNull],
+	c.is_identity,
+	c.collation_name
+FROM sys.all_columns AS c
+JOIN sys.types AS t
+ON c.system_type_id = t.system_type_id
+JOIN (SELECT NAME, OBJECT_ID FROM sys.tables) AS tb
+ON tb.object_id = c.object_id
+WHERE t.NAME != 'sysname'
+--AND tb.NAME = 'ticket'
+--AND t.name = 'nvarchar'
 
 
 /*----------------------ALL COLUMNS IMFORMATION----------------------*/
 
 SELECT
 	tb.NAME AS TableName,
-	t.NAME AS ColumnType,
+	t.NAME 	AS ColumnType,
 	c.*
 FROM sys.all_columns AS c
 JOIN sys.types AS t
@@ -29,3 +32,4 @@ JOIN (SELECT NAME, OBJECT_ID FROM sys.tables) AS tb
 ON tb.object_id = c.object_id
 WHERE t.NAME != 'sysname'
 --AND tb.NAME = 'ticket'
+--AND t.name = 'nvarchar'
