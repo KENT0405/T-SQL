d9ab3d5437fc89acdaeb0fe2a9dc1bec6feb45b5
kent <faec9408cc@gmail.com>
2023-07-20 15:20:49 +0800

    2023-07-20

--- Script/Search_procedure_view_text.sql
+++ Script/Search_procedure_view_text.sql
@@ -1,11 +1,12 @@
 SELECT DISTINCT
 	o.name,
+	o.type,
 	c.*
 FROM syscomments c
 INNER JOIN sysobjects o ON c.id=o.id
-WHERE(o.xtype = 'P' OR	o.xtype = 'V')
+WHERE(o.xtype = 'P' OR o.xtype = 'V' OR o.type = 'FN' OR o.type = 'TF' OR o.type = 'IF')
 And
 (
-		o.name LIKE '%特定文字%'	--查SP或View名稱
-	OR	c.text LIKE '%特定文字%'	--查SP或View內含文字
+		o.name LIKE '%特定文字%'	--查Procedure,View,Function名稱
+	OR	c.text LIKE '%特定文字%'	--查Procedure,View,Function內含文字
 )
