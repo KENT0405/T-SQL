3f97c51143a1c699a1e0fedb29d728747bc3b248
kent <faec9408cc@gmail.com>
2023-08-09 09:31:11 +0800

    2023-08-09

--- Script/## Alter_Grant.sql
+++ Script/## Alter_Grant.sql
@@ -3,3 +3,9 @@ GRANT VIEW DEFINITION TO hx
 
 --給 user 在某一張表的 alter 權限
 Grant alter on merchant_archive to fs_egame_user
+
+--給 hx EXECUTE的權限
+GRANT EXECUTE TO hx
+
+--給 hx 執行某一支procedure的權限
+GRANT EXECUTE ON PROCEDURE procedure_name TO hx;
