38dca769a6579afda36b28617352e23cbab301ac
kent <faec9408cc@gmail.com>
2023-08-09 18:12:15 +0800

    2023-08-09

--- Script/Clone DB/clone TB.sql
+++ Script/Clone DB/clone TB.sql
@@ -35,13 +35,13 @@ BEGIN
 			WHEN 'varchar' THEN '(' + CAST(isc.character_maximum_length AS VARCHAR(10)) + ')'
 		ELSE '' END +
 		CASE WHEN col.is_identity = 1 THEN ' IDENTITY(' + CAST(IDENT_SEED(@TB_name) AS VARCHAR(5)) + ',' + CAST(IDENT_INCR(@TB_name) AS VARCHAR(5)) + ')' ELSE '' END +
-		CASE WHEN isc.is_nullable = 'NO' THEN ' NOT NULL' ELSE ' NULL' END +
-		CASE WHEN (SELECT COUNT(1) FROM sys.indexes WHERE OBJECT_NAME(object_id) = @TB_name AND is_primary_key = 1) = 1 THEN ',' ELSE '' END + '
-		'
+		CASE WHEN isc.is_nullable = 'NO' THEN ' NOT NULL,' ELSE ' NULL,' END
 	FROM information_schema.columns isc
 	JOIN sys.tables tb ON isc.table_name = tb.name
 	JOIN sys.all_columns col ON tb.object_id = col.object_id AND isc.column_name = col.name
 	WHERE tb.name = @TB_name
+
+	SET @SQL_TB =
 
 	SELECT @SQL = '
 	CREATE TABLE [dbo].[' + @TB_name + '](
