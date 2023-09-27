4e6a617e7405540bb21fe6e0001b06476e34e723
kent <faec9408cc@gmail.com>
2023-09-27 17:50:14 +0800

    2023-09-27

--- Q&A/Begin tran _test.sql
+++ Q&A/Begin tran _test.sql
@@ -1,21 +1,23 @@
+DROP TABLE IF EXISTS PROC_TEST
 CREATE TABLE PROC_TEST
 (
-	sn int IDENTITY(1,1),
+	sn int PRIMARY KEY,
 	test VARCHAR(20)
 )
+GO
 -----------------------------------------------------------------------------
 -----------------------------------------------------------------------------
 
 
-CREATE OR ALTER PROC [dbo].[proc_test3] --1、2、3、4
-	@test INT
+CREATE OR ALTER PROC [dbo].[proc_test4] --1、2、3、4
+	@sn INT,
+	@test VARCHAR(10)
 AS
 BEGIN
-
 	BEGIN TRAN
 		BEGIN TRY
 
-			INSERT INTO PROC_TEST VALUES(@test)
+			INSERT INTO PROC_TEST VALUES(@sn, @test)
 
 			COMMIT
 		END TRY
@@ -25,43 +27,45 @@ BEGIN
 		EXEC dbo.up_sys_error_log
 
 	END CATCH
-
 END
 GO
 -----------------------------------------------------------------------------
 -----------------------------------------------------------------------------
 
-BEGIN TRAN
-BEGIN TRY
 
-	EXEC [dbo].[proc_test1] 'abc'
-	EXEC [dbo].[proc_test2] 'ABC'
-	EXEC [dbo].[proc_test3] 'abc'
-	EXEC [dbo].[proc_test4] 'abc'
+CREATE OR ALTER PROC tt
+AS
+BEGIN
+	DECLARE @i INT = 1
 
-	COMMIT
-END TRY
-BEGIN CATCH
+	WHILE(1=1)
+	BEGIN
+		BEGIN TRAN
 
-	ROLLBACK
-	EXEC dbo.up_sys_error_log
+		EXEC [dbo].[proc_test1] 1,'abc'
 
-END CATCH
+		EXEC [dbo].[proc_test2] 1,'sda' --error
 
-GO
+		EXEC [dbo].[proc_test3] 2,'abc'
 
+		EXEC [dbo].[proc_test4] 3,'asdasd'
+
+		COMMIT
+
+		IF (@i = 2)
+			BREAK;
+
+		SET @i += 1
+	END
+END
+
+/*
 SELECT *
 FROM dbo.sys_jobs_errormessage
 
 TRUNCATE TABLE dbo.sys_jobs_errormessage
------------------------------------------------------------------------------
------------------------------------------------------------------------------
-
-
---EXEC [dbo].[proc_test1] 'abc'
 
 SELECT * FROM PROC_TEST
 
 TRUNCATE TABLE PROC_TEST
-
---SELECT * FROM sys.messages WHERE message_id = 8114
+*/
