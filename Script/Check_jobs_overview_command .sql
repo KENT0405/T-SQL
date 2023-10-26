da5a3c44094b46d67c99a7badc716f875aca286b
kent <faec9408cc@gmail.com>
2023-10-26 18:10:31 +0800

    2023-10-26

--- Script/Check_jobs_overview_command .sql
+++ Script/Check_jobs_overview_command .sql
@@ -9,3 +9,53 @@ SELECT
 FROM sysjobs AS jobs
 INNER JOIN sysjobsteps AS steps ON jobs.job_id = steps.job_id
 ORDER BY JobName, StepID
+
+/* JOB 裡面的所有 procedure
+;WITH CTE
+AS
+(
+	SELECT s.value AS Segment
+	FROM msdb.dbo.sysjobs AS jobs
+	INNER JOIN msdb.dbo.sysjobsteps AS steps ON jobs.job_id = steps.job_id
+	CROSS APPLY STRING_SPLIT(steps.command, ' ') AS s
+),CTE2
+AS
+(
+	SELECT s.value AS Segment
+	FROM CTE CROSS APPLY STRING_SPLIT(Segment, '''') AS s
+),CTE3
+AS
+(
+	SELECT s.value AS Segment
+	FROM CTE2 CROSS APPLY STRING_SPLIT(Segment, '.') AS s
+),CTE4
+AS
+(
+	SELECT s.value AS Segment
+	FROM CTE3 CROSS APPLY STRING_SPLIT(Segment, '[') AS s
+),CTE5
+AS
+(
+	SELECT s.value AS Segment
+	FROM CTE4 CROSS APPLY STRING_SPLIT(Segment, ']') AS s
+),CTE6
+AS
+(
+	SELECT s.value AS Segment
+	FROM CTE5 CROSS APPLY STRING_SPLIT(Segment, CHAR(9)) AS s --CHAR(9) = 'TAB鍵'
+),CTE7
+AS
+(
+	SELECT s.value AS Segment
+	FROM CTE6 CROSS APPLY STRING_SPLIT(Segment, CHAR(10)) AS s --CHAR(10) = 'Enter鍵'
+),CTE8
+AS
+(
+	SELECT s.value AS Segment
+	FROM CTE6 CROSS APPLY STRING_SPLIT(Segment, CHAR(13)) AS s --CHAR(13) = 'Enter鍵'
+)
+SELECT DISTINCT name
+FROM sys.procedures p
+JOIN CTE8 c ON p.name = c.Segment
+ORDER BY name
+*/
