10604dcba9aacb9212b9f110e76b0ad4aedc54cc
kent <faec9408cc@gmail.com>
2024-05-14 14:47:52 +0800

    2024-05-14

--- Script/Search_text_sp_vw_fn.sql
+++ Script/Search_text_sp_vw_fn.sql
@@ -10,3 +10,19 @@ WHERE
 	OR	c.text LIKE '%特定文字%'	--查Procedure,View,Function內含文字
 )
 --AND (o.xtype = 'P' OR o.xtype = 'V' OR o.type = 'FN' OR o.type = 'TF' OR o.type = 'IF')
+
+
+/* 查找job中的procedure
+USE msdb;
+GO
+
+SELECT
+    j.name AS JobName,
+    s.step_id AS StepID,
+    s.step_name AS StepName,
+    s.command AS Command
+FROM sysjobs j
+INNER JOIN sysjobsteps s ON j.job_id = s.job_id
+WHERE s.command LIKE '%EXEC%'
+AND   s.command LIKE '%procedure_name%'; -- 用你要查詢的儲存程序名稱替換 'procedure_name'
+*/
