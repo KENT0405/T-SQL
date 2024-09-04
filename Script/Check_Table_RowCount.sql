2aaeb295f273ff03ce76979d97ffa9691e9028cd
kent <faec9408cc@gmail.com>
2024-09-04 11:48:12 +0800

    2024-09-04

--- Script/Check_Table_RowCount.sql
+++ Script/Check_Table_RowCount.sql
@@ -9,6 +9,7 @@ SELECT
 		WHEN 2 THEN 'PAGE'
 	ELSE CAST(data_compression AS VARCHAR(10)) END AS compression,
 	fill_factor
+	--CASE data_compression WHEN 0 THEN 'ALTER TABLE ' + t.NAME + ' REBUILD PARTITION = ALL WITH (DATA_COMPRESSION = PAGE)' END AS Alter_table_compression
 FROM sys.tables t
 INNER JOIN sys.indexes i ON t.OBJECT_ID = i.object_id
 INNER JOIN sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
@@ -20,6 +21,7 @@ AND t.is_ms_shipped = 0
 AND i.OBJECT_ID > 255
 AND p.rows > 0
 AND a.type_desc = 'IN_ROW_DATA'
+--AND data_compression = 0
 --AND modify_date <= '2020'
 --AND t.NAME like '%daily_tran%'
 GROUP BY t.Name,create_date,modify_date,data_compression,fill_factor
