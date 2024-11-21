524ba14e8ddf59ff9bba7913b8eeb0bdd2fe5d0c
kent <faec9408cc@gmail.com>
2024-11-21 14:19:58 +0800

    2024-11-21

--- Script/Check_jobs_overview_command .sql
+++ Script/Check_jobs_overview_command .sql
@@ -47,19 +47,24 @@ AS
 AS
 (
 	SELECT s.value AS Segment
-	FROM CTE5 CROSS APPLY STRING_SPLIT(Segment, CHAR(9)) AS s --CHAR(9) = 'TAB鍵'
+	FROM CTE5 CROSS APPLY STRING_SPLIT(Segment, ';') AS s
 ),CTE7
 AS
 (
 	SELECT s.value AS Segment
-	FROM CTE6 CROSS APPLY STRING_SPLIT(Segment, CHAR(10)) AS s --CHAR(10) = 'Enter鍵'
+	FROM CTE6 CROSS APPLY STRING_SPLIT(Segment, CHAR(9)) AS s --CHAR(9) = 'TAB鍵'
 ),CTE8
 AS
 (
 	SELECT s.value AS Segment
-	FROM CTE6 CROSS APPLY STRING_SPLIT(Segment, CHAR(13)) AS s --CHAR(13) = 'Enter鍵'
+	FROM CTE7 CROSS APPLY STRING_SPLIT(Segment, CHAR(10)) AS s --CHAR(10) = 'Enter鍵'
+),CTE9
+AS
+(
+	SELECT s.value AS Segment
+	FROM CTE8 CROSS APPLY STRING_SPLIT(Segment, CHAR(13)) AS s --CHAR(13) = 'Enter鍵'
 )
 SELECT DISTINCT name
 FROM sys.procedures p
-JOIN CTE8 c ON p.name = c.Segment
+JOIN CTE9 c ON p.name = c.Segment
 ORDER BY name
