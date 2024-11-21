9ecc4ad0dfe6afb90687847e19b4843dd54c4c61
kent <faec9408cc@gmail.com>
2024-11-21 14:08:20 +0800

    2024-11-21

--- Script/Check_jobs_overview_command .sql
+++ Script/Check_jobs_overview_command .sql
@@ -1,16 +1,14 @@
 ---------------------------------------------
 ---------- JOB overview command -------------
 ---------------------------------------------
-USE msdb
-
 SELECT
     jobs.name AS JobName,
     steps.step_id AS StepID,
     steps.step_name AS StepName,
     steps.subsystem AS Subsystem,
     steps.command AS Command
-FROM sysjobs AS jobs
-INNER JOIN sysjobsteps AS steps ON jobs.job_id = steps.job_id
+FROM msdb..sysjobs AS jobs
+INNER JOIN msdb..sysjobsteps AS steps ON jobs.job_id = steps.job_id
 --WHERE command LIKE '%Procedure_Name%'
 ORDER BY JobName, StepID
 
