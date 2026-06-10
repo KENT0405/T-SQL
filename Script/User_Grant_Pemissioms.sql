4b4cd5d0d00353a6d6e636422ae29f279671b4e8
kent <faec9408cc@gmail.com>
2026-06-10 09:54:17 +0800

    2026-06-10

--- Script/User_Grant_Pemissioms.sql
+++ Script/User_Grant_Pemissioms.sql
@@ -1,3 +1,12 @@
+SELECT
+    name,
+    'DENY CONNECT SQL TO [' + name + '];ALTER LOGIN [' + name + '] DISABLE;' as DENY_str,
+    'GRANT CONNECT SQL TO [' + name + '];ALTER LOGIN [' + name + '] ENABLE;' as GRANT_str
+FROM sys.sql_logins
+WHERE name NOT IN ('dbadmin','Db_user','repl_admin','##MS_PolicyTsqlExecutionLogin##','##MS_PolicyEventProcessingLogin##','gino','jacky','kent')
+
+
+/*
 --如果對方有datareader的權限，卻還是看不見procedure，執行以下語法
 GRANT VIEW DEFINITION TO hx
 
@@ -9,3 +18,4 @@ GRANT EXECUTE TO hx
 
 --給 hx 執行某一支procedure的權限
 GRANT EXECUTE ON procedure_name TO hx;
+*/
